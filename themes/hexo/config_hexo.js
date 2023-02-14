e3d423b194a83b67a0504fc223a8d589b8e02996
DANboy <72214248+dongla-danzeng@users.noreply.github.com>
2023-02-14 19:05:37 +0800

    Update config_hexo.js

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -1,6 +1,6 @@
 const CONFIG_HEXO = {
   HOME_BANNER_ENABLE: true,
-  HOME_BANNER_GREETINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
+  HOME_BANNER_GREETINGS: ['无论如何都不能丢掉良心与理想🎉', '自己闯荡得来的就比较有成就感🎉', '愿所有的努力都能配得上当当当初的野心🎉', '欢迎来到我的博客🎉'], // 首页大图标语文字
 
   HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
 

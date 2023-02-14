bc65d732e05097c7350f6b5cbc8c40236fd93b72
DANboy <72214248+dongla-danzeng@users.noreply.github.com>
2023-02-14 17:18:35 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -10,17 +10,17 @@ const BLOG = {
   SINCE: 2021, // e.g if leave this empty, current year will be used.
   APPEARANCE: 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
 
-  AUTHOR: 'DANboy——ངག་བསྟན་', // 您的昵称 例如 tangly1024
-  BIO: '一个普通的体制内员工的随笔录站🍚', // 作者简介
+  AUTHOR: 'DANboy-ངག་བསྟན་', // 您的昵称 例如 tangly1024
+  BIO: '一个普通de体制内员工的随笔录站🍚', // 作者简介
   LINK: 'https://danboy-1518.top', // 网站地址
-  KEYWORDS: 软件分享 |, Ubuntu |, 软件无线电 |, Java | , 读书分享 |,硬件知识分享 |', // 网站关键词 英文逗号隔开
+  KEYWORDS: '软件分享, 硬件分享, 软件无线电, Java , Python,读书分享', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: 'danboywy@163.com', // 邮箱
+  CONTACT_EMAIL: 'danboywy@163.com', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: 'https://weibo.com/u/7009382219', // 你的微博个人主页
   //CONTACT_TWITTER: '', // 你的twitter个人主页
-  CONTACT_GITHUB: 'https://github.com/dongla-danzeng', // 你的github个人主页
-//  CONTACT_TELEGRAM: 'https://t.me/tangly_1024', // 你的telegram 地址 例如 https://t.me/tangly_1024
- // CONTACT_LINKEDIN: '', // 你的linkedIn 首页
+  CONTACT_GITHUB: 'https://github.com/dongla-danzeng', // 你的github个人主页 例如 https://github.com/tangly1024
+  //CONTACT_TELEGRAM: '', // 你的telegram 地址 例如 https://t.me/tangly_1024
+  //CONTACT_LINKEDIN: '', // 你的linkedIn 首页
 
   // 网站字体
   FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-serif', // ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115

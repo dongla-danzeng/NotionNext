52f01d50f624023787a7bebef1f1e49468acf6bf
DANboy <72214248+dongla-danzeng@users.noreply.github.com>
2023-02-14 21:37:17 +0800

    Update Twikoo.js

--- components/Twikoo.js
+++ components/Twikoo.js
@@ -23,31 +23,5 @@ const Twikoo = ({ isDarkMode }) => {
    <div id="twikoo"></div>
   )
 }
-twikoo.getRecentComments({
-  envId: BLOG.COMMENT_TWIKOO_ENV_ID , // 环境 ID
-  // region: 'ap-guangzhou', // 环境地域，默认为 ap-shanghai，如果您的环境地域不是上海，需传此参数
-  pageSize: 10, // 获取多少条，默认：10，最大：100
-  includeReply: false // 是否包括最新回复，默认：false
-}).then(function (res) {
-  console.log(res);
-  // 返回 Array，包含最新评论的
-  //   * id:           评论 ID
-  //   * url:          评论地址
-  //   * nick:         昵称
-  //   * mailMd5:      邮箱的 MD5 值，可用于展示头像
-  //   * link:         网址
-  //   * comment:      HTML 格式的评论内容
-  //   * commentText:  纯文本格式的评论内容
-  //   * created:      评论时间，格式为毫秒级时间戳
-  //   * avatar:       头像地址（0.2.9 新增）
-  //   * relativeTime: 相对评论时间，如 “1 小时前”（0.2.9 新增）
-  // 返回示例: [ // 从新到旧顺序
-  //   { id: '', url: '', nick: '', mailMd5: '', link: '', comment: '', commentText: '', created: 0 },
-  //   { id: '', url: '', nick: '', mailMd5: '', link: '', comment: '', commentText: '', created: 0 },
-  //   { id: '', url: '', nick: '', mailMd5: '', link: '', comment: '', commentText: '', created: 0 }
-  // ]
-}).catch(function (err) {
-  // 发生错误
-  console.error(err);
-});
+
 export default Twikoo

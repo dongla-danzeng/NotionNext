61a646acc8a908e39174c4fe9322316232d3c6de
DANboy <72214248+dongla-danzeng@users.noreply.github.com>
2023-02-14 21:34:39 +0800

    Update Twikoo.js

--- components/Twikoo.js
+++ components/Twikoo.js
@@ -23,50 +23,31 @@ const Twikoo = ({ isDarkMode }) => {
    <div id="twikoo"></div>
   )
 }
-//判断网址,防止访客网址不写http和https
-       function wangzhi(e){
-        http = e.slice(0,4)
-        https = e.slice(0,5)
-        if (http == "http" || https == "https" ){
-             return e
-        } else if (e == "" || e == null || e == undefined){
-          return e
-        } else {
-          e = 'http://'+ e
-          return e
-        }
-    }
- 
-//调用twikoo最新评论主函数
-function newcomment() {
-    twikoo.getRecentComments({
-        envId: BLOG.COMMENT_TWIKOO_ENV_ID, // 环境 ID
-        pageSize: 7, // 获取多少条，默认：10，最大：100
-        includeReply: false // 是否包括最新回复，默认：false
-    }).then(function (res) {
-        console.log(res);
-        var hotComments = $("#hot-comments");
-        for (var i = 0; i < res.length; i++) {
-            if (i === 0) {
-                console.log(res[0]);
-            }
-            var nick = res[i].nick;//访客姓名
-            var content = res[i].commentText;//评论内容
-            var newcontent = content.substring(0, 50); //字数截取后评论内容
-            var url = res[i].url;//文章链接
-            var avatar = res[i].avatar;//评论者头像
-            var link = wangzhi(res[i].link);//评论者网址
-            var updatedAt = res[i].relativeTime;//评论时间
-            var commentId = '#' + res[i].id;//评论id
-            hotComments.append('<li class="px1 pb2 flex items-center"><img style="width: 40px;height:40px" class="circle mx1 listavatar" src="' + avatar + '"><div class="w100"><div class="flex justify-between"><div class="h6 listauthor overflow-hidden" title="' + nick + '"><a  target="_blank" rel="noopener external nofollow noreferrer" href="' + link + '">' + nick + '</a></div><span class="h6 mr1 listdate wenzi hang1">' + updatedAt + '</span></div> <a href="' + url + commentId + '"><div class="h5 list-comcontent overflow-hidden">' + newcontent + '</div></a></div></li>');
-        }
-    }).catch(function (err) {
-        console.error(err);
-    });
-}
- 
-$(function () {
-    newcomment();//调用newcomment
+twikoo.getRecentComments({
+  envId: BLOG.COMMENT_TWIKOO_ENV_ID , // 环境 ID
+  // region: 'ap-guangzhou', // 环境地域，默认为 ap-shanghai，如果您的环境地域不是上海，需传此参数
+  pageSize: 10, // 获取多少条，默认：10，最大：100
+  includeReply: false // 是否包括最新回复，默认：false
+}).then(function (res) {
+  console.log(res);
+  // 返回 Array，包含最新评论的
+  //   * id:           评论 ID
+  //   * url:          评论地址
+  //   * nick:         昵称
+  //   * mailMd5:      邮箱的 MD5 值，可用于展示头像
+  //   * link:         网址
+  //   * comment:      HTML 格式的评论内容
+  //   * commentText:  纯文本格式的评论内容
+  //   * created:      评论时间，格式为毫秒级时间戳
+  //   * avatar:       头像地址（0.2.9 新增）
+  //   * relativeTime: 相对评论时间，如 “1 小时前”（0.2.9 新增）
+  // 返回示例: [ // 从新到旧顺序
+  //   { id: '', url: '', nick: '', mailMd5: '', link: '', comment: '', commentText: '', created: 0 },
+  //   { id: '', url: '', nick: '', mailMd5: '', link: '', comment: '', commentText: '', created: 0 },
+  //   { id: '', url: '', nick: '', mailMd5: '', link: '', comment: '', commentText: '', created: 0 }
+  // ]
+}).catch(function (err) {
+  // 发生错误
+  console.error(err);
 });
-
 export default Twikoo
